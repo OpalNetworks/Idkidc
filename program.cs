89d6a2805302e89817c248064716adca76cbb348
OpalNetworks <opal.ephtv.networks@hotmail.com>
2024-08-26 18:13:59 -0400

    Update program.cs

--- program.cs
+++ program.cs
@@ -3,69 +3,54 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Net.Http;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 
-namespace ABREEProxy
+public class Program
 {
-    public class Startup
+    public static void Main(string[] args)
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
-        }
+        CreateHostBuilder(args).Build().Run();
+    }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
             {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.SendFileAsync("wwwroot/index.html");
-                });
-
-                endpoints.MapPost("/search", async context =>
-                {
-                    var query = context.Request.Form["query"].ToString();
-                    var searchResult = await ProxySearchRequest(query);
-
-                    context.Response.ContentType = "text/html";
-                    await context.Response.WriteAsync(searchResult);
-                });
+                webBuilder.UseStartup<Startup>();
             });
-        }
+}
 
-        private async Task<string> ProxySearchRequest(string query)
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+        services.AddHttpClient();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-            using (HttpClient client = new HttpClient())
-            {
-                // Fetch the search results using a search engine like Bing or DuckDuckGo
-                var response = await client.GetAsync($"https://duckduckgo.com/?q={System.Net.WebUtility.UrlEncode(query)}");
-
-                if (response.IsSuccessStatusCode)
-                {
-                    return await response.Content.ReadAsStringAsync();
-                }
-                else
-                {
-                    return "<h1>Error fetching search results</h1>";
-                }
-            }
+            app.UseDeveloperExceptionPage();
         }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
     }
 }
